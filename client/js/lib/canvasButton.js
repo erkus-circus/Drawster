6b123a34add5d8b45b104125f267917b9e96fc28
eric diskin <ericdiskin26@gmail.com>
2020-05-27 22:36:42 -0400

    FUUUUUUUUUDGE

---
+++ client/js/lib/canvasButton.js
@@ -0,0 +1,10 @@
+(function (window) {
+    var ctx;
+    class ClickPoint {
+        x = 0;
+        y = 0;
+        constructor(x, y, draggable, clickCall, dragCall) {
+
+        }
+    }
+})(window);
--- client/js/tools/transform.js
+++ client/js/tools/transform.js
@@ -1,5 +1,6 @@
 (function (window) {
-
+    var start;
+    var end;
     /*
             var options = {
                 pos: pos,
@@ -25,20 +26,29 @@
     };
 
     Project.prototype.Draw.Transform.Move = function (opts) {
+        var tc = opts.topCtx;
+        var { pageOpen } = opts.config;
 
+        Project.Draw.clear(tc);
+
+        if (pageOpen == "move") {
+            end[0] += opts.deltaX;
+            end[1] += opts.deltaY;
+            tc.fillRect(...start, end[0] - start[0], end[1] - start[1]);
+        }
     };
 
     Project.prototype.Draw.Transform.Init = function (opts) {
+        if (!Project.Draw.Select.selected) {
+            // ERROR
+            return;
+        }
         Project.Draw.Select.selected = false;
-    }
+        start = Project.Draw.Select.topPos;
+        end = Project.Draw.Select.bottomPos;
+        
+    Project.prototype.Draw.Transform.Deselect = function (opts) { }
 
-    Project.prototype.Draw.Transform.Deselect = function (opts) {}
-
-    Project.prototype.Draw.Transform.Up = function (opts) {
-    };
-
-
-
-
-    $(window).load(function () {});
+    Project.prototype.Draw.Transform.Up = function (opts) { };
+    $(window).load(function () { });
 })(window);
